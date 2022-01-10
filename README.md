f5bd6d2595af60f5899523843957e571b49237b5
Brad Gibbons <bradley0531@msn.com>
2022-01-10 15:11:55 -0600

    Add Slack Notify Deploy action

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # github-action-slack-notify-deploy
-Github action to send a Slack message during a deploy
+
+Github action to send a Slack message during a deploy. Specifies the product being deployed, what version is being deployed, the environment being deployed to, and the status of the deployment.
+
+## Usage
+
+```
+uses: spotlight-ar/github-action-slack-notify-deploy@v1.0
+with:
+  channel: 123456
+  status: STARTED
+  product: Client
+  version: v1.0.0
+  environment: Production
+  environment_url: https://production.example.com
+env:
+  SLACK_BOT_TOKEN: ${{ secrets.SLACK_BOT_TOKEN }}
+```
+
+<img src="docs/example.png" alt="Screenshot of message">
+
+## Variables
+
+### Inputs:
+
+| Variable        | Description                                       | Required? |
+| --------------- | ------------------------------------------------- | --------- |
+| channel_id      | The Slack channel ID to send the message to.      | true      |
+| product         | The name of the product being deployed.           | true      |
+| version         | The version of the product being deployed.        | true      |
+| environment     | The environment the product is being deployed to. | true      |
+| environment_url | The URL for the environment being deployed to.    | false     |
+| status          | The status to list in the update.                 | true      |
+| message_id      | The ID of the existing Slack message to update.   | false     |
+
+### Outputs:
+
+| Variable   | Description                                                |
+| ---------- | ---------------------------------------------------------- |
+| message_id | The unique timestamp identifier of the Slack message sent. |
+
+### Secrets:
+
+| Variable        | Description                                |
+| --------------- | ------------------------------------------ |
+| SLACK_BOT_TOKEN | The Slackbot token to use for this actions |
---
+++ index.js
@@ -0,0 +1,103 @@
+const core = require('@actions/core');
+const { context } = require('@actions/github');
+const { WebClient } = require('@slack/web-api');
+
+const {
+  SLACK_BOT_TOKEN,
+  GITHUB_RUN_ID,
+} = process.env;
+
+const getInputs = () => {
+  return {
+    channelId: core.getInput('channel_id'),
+    product: core.getInput('product'),
+    version: core.getInput('version'),
+    environment: core.getInput('environment'),
+    envUrl: core.getInput('environment_url'),
+    status: core.getInput('status'),
+    messageId: core.getInput('message_id'),
+  }
+}
+
+const buildMessage = () => {
+
+}
+
+(async () => {
+  try {
+    const {
+      channelId,
+      product,
+      version,
+      environment,
+      envUrl,
+      status,
+      color,
+      messageId,
+    } = getInputs();
+
+    const runId = parseInt(GITHUB_RUN_ID, 10);
+    const { owner, repo } = context.repo;
+
+    const slack = new WebClient(SLACK_BOT_TOKEN);
+
+    const method = Boolean(messageId) ? 'update' : 'postMessage';
+
+    const args = {
+      channel: channelId,
+      icon_url: 'https://github.githubassets.com/favicon.ico',
+      username: 'Github Deploy Bot',
+      blocks: [
+        {
+          type: 'section',
+          fields: [
+            {
+              type: 'mrkdwn',
+              text: `*Product:*\n<https://github.com/${owner}/${repo} | ${product}>`,
+            },
+            {
+              type: 'mrkdwn',
+              text: `*Version:*\n<https://github.com/${owner}/${repo}/releases/tag/${version} | ${version}>`,
+            },
+            {
+              type: 'mrkdwn',
+              text: `*Environment:*\n${environment}`,
+            },
+            {
+              type: 'mrkdwn',
+              text: `*Status:*\n${status}`,
+            },
+          ],
+        },
+        {
+          type: 'context',
+          elements: [
+            {
+              type: 'image',
+              image_url: 'https://github.githubassets.com/favicon.ico',
+              alt_text: 'github',
+            },
+            {
+              type: 'mrkdwn',
+              text: `Triggered from <https://github.com/${owner}/${repo}/actions/runs/${runId} | ${owner}/${repo}>`,
+            },
+          ],
+        },
+      ],
+    }
+
+    if (envUrl) {
+      args.blocks[0].fields[2].text = `*Environment:*\n<${envUrl} | ${environment}>`;
+    }
+
+    if (messageId) {
+      args.ts = messageId;
+    }
+
+    const response = await slack.chat[method](args);
+
+    core.setOutput('message_id', response.ts);
+  } catch (err) {
+    core.setFailed(err);
+  }
+})();
